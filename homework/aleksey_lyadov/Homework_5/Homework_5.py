9c27897d8769a34d8935a698623d3951a6a74171
Лядов Алексей Валентинович <a.lyadov@smarthorizon.ru>
2024-12-26 09:18:14 +0300

    Домашка по 5-му уроку, исправления по заданию 2

--- homework/aleksey_lyadov/Homework_5/Homework_5.py
+++ homework/aleksey_lyadov/Homework_5/Homework_5.py
@@ -7,15 +7,18 @@ print(name, last_name, city, phone, country)
 a = 'результат операции: 42'
 b = 'результат операции: 514'
 c = 'результат работы программы: 9'
-index_a = a.index('42')
-index_b = b.index('514')
-index_c = c.index('9')
+# Т.к. мы не знаем какие цифры у нас могут быть в строке, но у нас есть общий формат строки, то я закрепился за ":",
+# сама строка тоже может быть нестандартной. Получается я по индексу ищу ":" потом прибавляю 2 знака, чтобы счет начался
+# с числа, далее перевожу в формат int чтобы полученные числа можно было сложить.
+index_a = a.index(':') + 2
+index_b = b.index(':') + 2
+index_c = c.index(':') + 2
 int_a = int(a[index_a:])
 int_b = int(b[index_b:])
 int_c = int(c[index_c:])
 print(int_a + 10, int_b + 10, int_c + 10)
 
-# Задание №2
+# Задание №3
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 students = ', '.join(students)
