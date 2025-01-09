d6bfb33793d6d14ed85efb00895bbc5f1e1767d7
Лядов Алексей Валентинович <a.lyadov@smarthorizon.ru>
2025-01-09 15:49:22 +0300

    Домашка по 6-му уроку

---
+++ homework/aleksey_lyadov/Homework_6/Homework_6_1.py
@@ -0,0 +1,9 @@
+# Задание №1
+texts = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+         'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+
+format_text = texts.split()
+for text in format_text:
+    text_ing = 'ing' + text
+    print(text_ing, end= " ")
+
