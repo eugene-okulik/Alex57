1072f690fef5ffe08c13893623445310c2921b35
Лядов Алексей Валентинович <a.lyadov@smarthorizon.ru>
2025-01-10 15:12:45 +0300

    исправления задания 1 по домашке 6-го урока

--- homework/aleksey_lyadov/Homework_6/Homework_6_1.py
+++ homework/aleksey_lyadov/Homework_6/Homework_6_1.py
@@ -1,8 +1,15 @@
 # Задание №1
 texts = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
-         'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+        'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 
 format_text = texts.split()
+fin_text = []
 for text in format_text:
-    text_ing = 'ing' + text
-    print(text_ing, end=" ")
+    if text[-1] in [',', '.']:
+        fin_text.append(text[:-1] + 'ing' + text[-1])
+    else:
+        fin_text.append(text + 'ing')
+join_text = ' '.join(fin_text)
+print(join_text)
+
+
