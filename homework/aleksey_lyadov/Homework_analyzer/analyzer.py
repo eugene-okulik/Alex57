0f3c12d00763352f7619141135e42cc9653e22ce
Лядов Алексей Валентинович <a.lyadov@smarthorizon.ru>
2025-03-29 22:39:00 +0300

    правка flake8

--- homework/aleksey_lyadov/Homework_analyzer/analyzer.py
+++ homework/aleksey_lyadov/Homework_analyzer/analyzer.py
@@ -23,32 +23,36 @@ def find_text_in_files():
 
                     # Разбиваем строку на слова с сохранением позиций
                     words = []
-                    for m in re.finditer(r'\b(\w+)\b', line):  # Ищем только слова
+                    # Ищем только слова
+                    for m in re.finditer(r'\b(\w+)\b', line):
                         words.append((m.start(), m.end(), m.group()))
 
                     for match in matches:
-                        start_pos, end_pos = match.start(), match.end()
+                        start_pos = match.start()
 
                         # Находим индекс слова, содержащего совпадение
                         match_word_idx = next(
                             (i for i, (w_start, w_end, _) in enumerate(words)
-                             if w_start <= start_pos < w_end),-1)
+                             if w_start <= start_pos < w_end), -1)
 
                         if match_word_idx == -1:
                             continue
 
                         # Определяем диапазон слов для контекста
                         start_idx = max(0, match_word_idx - 5)
-                        end_idx = min(len(words), match_word_idx + 6)  # +1 для включения 5 слов после
+                        # +1 для включения 5 слов после
+                        end_idx = min(len(words), match_word_idx + 6)
 
-                        # Собираем контекст с сохранением оригинальных пробелов между словами
-                        context_parts = [words[start_idx][2]]  # Начинаем с первого слова
+                        # Собираем контекст с сохранением
+                        # оригинальных пробелов между словами
+                        context_parts = [words[start_idx][2]]
 
                         # Добавляем остальные слова с пробелами между ними
                         for i in range(start_idx + 1, end_idx):
                             prev_end = words[i - 1][1]
                             curr_start = words[i][0]
-                            context_parts.append(line[prev_end:curr_start])  # Пробелы между словами
+                            # Пробелы между словами
+                            context_parts.append(line[prev_end:curr_start])
                             context_parts.append(words[i][2])  # Само слово
 
                         context = ''.join(context_parts).strip()
